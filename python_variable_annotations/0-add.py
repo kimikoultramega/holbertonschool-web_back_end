2059c78cc5af8bfaa0b36906db5adf2a8491c725
kimikoultramega <9379@holbertonstudents.com>
2025-04-25 10:37:59 -0300

    wako

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+0-add module
+Provee una función `add` que suma dos floats.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Suma dos valores de tipo float.
+
+    Args:
+        a (float): primer sumando.
+        b (float): segundo sumando.
+
+    Returns:
+        float: resultado de a + b.
+    """
+    return a + b
