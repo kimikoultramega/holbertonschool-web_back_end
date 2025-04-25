51944d9590cb798fea6490a40b23fc31cc3b83da
kimikoultramega <9379@holbertonstudents.com>
2025-04-25 13:17:35 -0300

    git push

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Algo bonito
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Retorna la suma de una lista de floats
+    """
+    return sum(input_list)
