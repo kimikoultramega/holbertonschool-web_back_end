e3f30b7df737e61ebc853226e0e31ab4e5454d7e
kimikoultramega <9379@holbertonstudents.com>
2025-04-25 13:33:34 -0300

    robot

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Dañe, poño.
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Complicated.
+    """
+    return (k, v * v)
