0470def6777f122cbde969b9a69e728364f2b383
kimikoultramega <9379@holbertonstudents.com>
2025-04-25 13:26:38 -0300

    un flush

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Algo wacamole
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Sumas mixeadas para mas sabor
+    """
+    return float(sum(mxd_lst))
