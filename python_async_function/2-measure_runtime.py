d1c998eeaaef254d2d327505d28d7cc3c9083d5c
kimikoultramega <9379@holbertonstudents.com>
2025-04-28 11:16:57 -0300

    again

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Nuevamente en el ruedo
+"""
+import time
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Ejecuta wait_n(n. max_delay)
+    """
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.time()
+    total_time = end - start
+    return total_time / n
