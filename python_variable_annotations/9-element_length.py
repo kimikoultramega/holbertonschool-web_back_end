8bd80054c550c40f29c3eab6545edd10cb884308
kimikoultramega <9379@holbertonstudents.com>
2025-04-28 08:59:53 -0300

    push

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -4,6 +4,7 @@ La montaña.
 """
 from typing import Iterable, Sequence, List, Tuple
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     La montaña está dura.
