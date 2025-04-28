1814cc0c394b4df2339085c6c25e5aa02b5bf273
kimikoultramega <9379@holbertonstudents.com>
2025-04-28 09:49:45 -0300

    cameo

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,20 +4,21 @@ Courtine
 """
 import asyncio
 from typing import List
-wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    This function sorts the contents of a list of floats
-
-    args: n - number, max_delay - max number the function
-
-    Return: sorted list
+    Anda a saber.
     """
-    listfloat = []
+    # Crea las tareas
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
 
-    for i in range(n):
-        listfloat.append(wait_random(max_delay))
-    x = await asyncio.gather(*listfloat)
-    return sorted(x)
+    # Recoge los resultados según se completen
+    delays: List[float] = []
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+
+    return delays
