68970654a7a2e5a5b8137ba8f3040f84cee93ea2
kimikoultramega <9379@holbertonstudents.com>
2025-04-29 13:11:24 -0300

    yeah

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,9 +4,13 @@ Dale palo, que arrancamos.
 """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
+    """
+    En el proceso de aprendizaje.
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
