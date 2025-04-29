83e96f54ff0bd06fd3bb68b17cda5084dbeaa373
kimikoultramega <9379@holbertonstudents.com>
2025-04-29 13:04:31 -0300

    wakos

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,10 +3,11 @@
 Anderson - Bip Bop [So·leá 01·030]
 """
 import asyncio
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     Dipsy Funk - Pomelo Negroni [TAN009]
     """
