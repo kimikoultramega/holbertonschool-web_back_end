a8047c5bf8543d320cb9f5f9a2ac31040134f619
kimikoultramega <9379@holbertonstudents.com>
2025-04-29 10:24:12 -0300

    wako

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Dale palo, que arrancamos.
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
