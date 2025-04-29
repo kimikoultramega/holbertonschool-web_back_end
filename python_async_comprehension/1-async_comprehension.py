683bc054a51b7e309e40fc9adef8975257e77d64
kimikoultramega <9379@holbertonstudents.com>
2025-04-29 12:06:11 -0300

    Dipsy Funk - Pomelo Negroni [TAN009]

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Anderson - Bip Bop [So·leá 01·030]
+"""
+import asyncio
+from 0-async_generator import async_generator
+
+
+async def async_comprehension():
+    """
+    Dipsy Funk - Pomelo Negroni [TAN009]
+    """
+    result = []
+    async for item in async_generator():
+        result.append(item)
+    return result
