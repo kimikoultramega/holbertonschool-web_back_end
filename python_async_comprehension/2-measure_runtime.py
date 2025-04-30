013118c6b491f86ef0d73a62c348d3c321fc8aa9
kimikoultramega <9379@holbertonstudents.com>
2025-04-30 10:42:33 -0300

    owen

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -9,14 +9,12 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
+    """
+    tINI and the gang presents: Melody and Dr Banana
+    """
     start = time.perf_counter()  # Iniciamos el contador de tiempo
     #  Ejecutamos las 4 tareas en paralelo
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
     end = time.perf_counter()  # Detenemos el kronometro
 
     #  Calcular la duración total
