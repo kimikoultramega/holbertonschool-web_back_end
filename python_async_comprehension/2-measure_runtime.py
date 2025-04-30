b3591368d57ef1517ad576534f7171ca143ae245
kimikoultramega <9379@holbertonstudents.com>
2025-04-30 10:37:08 -0300

    saca

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Best of Rimsky-Korsakov - Classical Music Gems
+"""
+import asyncio
+import time
+from typing import Callable
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    start = time.perf_counter()  # Iniciamos el contador de tiempo
+    #  Ejecutamos las 4 tareas en paralelo
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end = time.perf_counter()  # Detenemos el kronometro
+
+    #  Calcular la duración total
+    duration = end - start
+    return duration
