a0727723f89c8ada68920c0d4d69fe6d6643292d
kimikoultramega <9379@holbertonstudents.com>
2025-05-30 09:46:06 -0300

    hey

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,37 +1,20 @@
-const fs = require('fs');
+const readline = require('readline');
 
-function countStudents(path) {
-  try {
-    const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
-    const studentLines = lines.slice(1);
-    const students = [];
-    studentLines.forEach((line) => {
-      const fields = line.split(',');
-      if (fields.length >= 4 && fields[0].trim() !== '') {
-        students.push({
-          firstname: fields[0].trim(),
-          lastname: fields[1].trim(),
-          age: fields[2].trim(),
-          field: fields[3].trim(),
-        });
-      }
-    });
-    console.log(`Number of students: ${students.length}`);
-    const fieldGroups = {};
-    students.forEach((student) => {
-      if (!fieldGroups[student.field]) {
-        fieldGroups[student.field] = [];
-      }
-      fieldGroups[student.field].push(student.firstname);
-    });
-    Object.keys(fieldGroups).forEach((field) => {
-      const studentNames = fieldGroups[field];
-      console.log(`Number of students in ${field}: ${studentNames.length}. List: ${studentNames.join(', ')}`);
-    });
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
-}
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout 
+});
 
-module.exports = countStudents;
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.question('', (name) => {
+
+  console.log(`Your name is: ${name}`);
+  
+  rl.close();
+});
+
+rl.on('close', () => {
+  console.log('This important software is now closing');
+  process.exit(0);
+});
