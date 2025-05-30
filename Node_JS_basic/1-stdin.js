1f0580ab6158c3d8fa0a417201e4c3a2137ccfc1
kimikoultramega <9379@holbertonstudents.com>
2025-05-30 09:48:58 -0300

    gir

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,19 +2,16 @@ const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout 
+  output: process.stdout
 });
 
 console.log('Welcome to Holberton School, what is your name?');
 
 rl.question('', (name) => {
-
   console.log(`Your name is: ${name}`);
-  
-  rl.close();
 });
 
-rl.on('close', () => {
-  console.log('This important software is now closing');
+process.on('SIGINT', () => {
+  console.log('\nThis important software is now closing');
   process.exit(0);
 });
