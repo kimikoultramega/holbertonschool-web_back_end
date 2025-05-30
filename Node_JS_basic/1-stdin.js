133ba88007358f9f27f0e111b47fa8ca68345320
kimikoultramega <9379@holbertonstudents.com>
2025-05-30 09:50:42 -0300

    yeah

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -8,7 +8,7 @@ const rl = readline.createInterface({
 console.log('Welcome to Holberton School, what is your name?');
 
 rl.question('', (name) => {
-  console.log(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${name}\r`);
 });
 
 process.on('SIGINT', () => {
